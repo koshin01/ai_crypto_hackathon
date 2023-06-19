9d7517fcc5e48995ce4ce8e4269c72fb612e8c9b
koshin <ekoshin2001@icloud.com>
2023-06-20 03:43:54 +0900

    ダイアログのタイトルの文字ウェイトを変更しました。

--- components/customDialog.js
+++ components/customDialog.js
@@ -71,7 +71,7 @@ export default function CustomDialog({ title, questionNum}) {
                                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                                     <Dialog.Title
                                         as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-900"
+                                        className="text-lg font-semibold leading-6 text-gray-900"
                                     >
                                         {title}
                                     </Dialog.Title>
